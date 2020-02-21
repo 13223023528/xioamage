ce598cf7e101150ac3c603bc3fab3ba61bf73448
wmy <2270430703@qq.com>
2020-02-21 15:15:57 +0800

    第三次提交

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="back-top">
+     <img src="~assets/img/common/top.png" alt="">
+   </div>
+</template>
+
+<script>
+export default {
+   data () {
+       return {
+
+       }
+   }
+}
+</script>
+
+<style scoped>
+.back-top {
+  position: fixed;
+  right: 5px;
+  bottom: 45px;
+}
+.back-top img {
+  width: 45px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,23 +3,33 @@
     <nav-bar class="home-nav-bar">
       <span slot="center">购物街</span>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <home-recommend :recommends="recommends"></home-recommend>
-    <home-popular></home-popular>
-    <tab-nav class="tab-nav" :title="['流行', '新款', '精选']" @tabClick="tabClick" />
-    <goods-list :goods="goods[currentType].list" />
+    <scroll class="content"
+            ref="scroll"
+            :probeType="3"
+            @scroll="contentScroll"
+            :pullUpLoad="true"
+            @pullingUp="loadMore"
+    >
+      <home-swiper :banners="banners"></home-swiper>
+      <home-recommend :recommends="recommends"></home-recommend>
+      <home-popular></home-popular>
+      <tab-nav class="tab-nav" :title="['流行', '新款', '精选']" @tabClick="tabClick" />
+      <goods-list :goods="goods[currentType].list" />
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTo" />
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import TabNav from "components/content/tabNav/TabNav";
-import GoodsList from "components/content/goods/GoodsList"
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop";
 
 import HomeSwiper from "./homeComponents/HomeSwiper";
 import HomeRecommend from "./homeComponents/HomeRecommend";
 import HomePopular from "./homeComponents/HomePopular";
-
 
 import { getMultiData, getProductData } from "network/home.js";
 export default {
@@ -29,11 +39,12 @@ export default {
       banners: [],
       recommends: [],
       goods: {
-        'pop': { page: 0, list: [] },
-        'new': { page: 0, list: [] },
-        'sell': { page: 0, list: [] }
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
       },
-      currentType: 'pop'
+      currentType: "pop",
+      isShowBackTo: false
     };
   },
   components: {
@@ -42,7 +53,9 @@ export default {
     HomeRecommend,
     HomePopular,
     TabNav,
-    GoodsList
+    GoodsList,
+    Scroll,
+    BackTop
   },
   created() {
     this.getMultiData();
@@ -62,7 +75,7 @@ export default {
       const page = this.goods[type].page + 1;
       getProductData(type, page).then(res => {
         this.goods[type].list.push(...res.data.list);
-      })
+      });
       this.goods[type].page++;
     },
 
@@ -70,15 +83,28 @@ export default {
     tabClick(index) {
       switch (index) {
         case 0:
-          this.currentType = 'pop';
+          this.currentType = "pop";
           break;
         case 1:
-          this.currentType = 'new';
+          this.currentType = "new";
           break;
         case 2:
-          this.currentType = 'sell';
+          this.currentType = "sell";
           break;
       }
+    },
+    // 返回顶部按钮
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0, 500);
+    },
+    // 判断返回顶部按钮是否显示
+    contentScroll(position) {
+      this.isShowBackTo = (-position.y) > 1000;
+    },
+    // 上拉加载更多
+    loadMore() {
+      this.getProductData(this.currentType);
+      this.$refs.scroll.finishPullUp();
     }
   }
 };
@@ -87,6 +113,7 @@ export default {
 <style scoped>
 #home {
   width: 100%;
+  height: 100vh;
   padding-top: 44px;
 }
 .home-nav-bar {
@@ -99,10 +126,11 @@ export default {
   z-index: 1;
 }
 .tab-nav {
-  position: sticky;
-  top: 44px;
   background: #fff;
   margin-bottom: 10px;
-  z-index: 1;
+}
+.content {
+  overflow: hidden;
+  height: calc(100% - 49px);
 }
 </style>
